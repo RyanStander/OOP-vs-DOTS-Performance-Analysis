6b4f91b784ce238ee771a6755be82a5df99294e8
Ryan <stander.ryan@gmail.com>
2023-06-04 19:15:12 +0200

    Working simple object moving

--- OOP vs DOTS/Assets/Scripts/ECS/MovingEntities/MoverAuthoring.cs
+++ OOP vs DOTS/Assets/Scripts/ECS/MovingEntities/MoverAuthoring.cs
@@ -1,11 +1,16 @@
 ﻿using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace ECS.MovingEntities
 {
+    /// <summary>
+    /// This is the entity of the mover.
+    /// </summary>
     public class MoverAuthoring : MonoBehaviour
     {
         public GameObject Object;
+        public Transform Transform;
         public float Speed;
     }
 
@@ -19,6 +24,12 @@ namespace ECS.MovingEntities
                 //By default, each authoring GameObject turns into an Entity.
                 //Given a GameObject (or authoring component), GetEntity looks up the resulting Entity.
                 Object = GetEntity(authoring.Object, TransformUsageFlags.Dynamic),
+                Transform = new LocalTransform
+                {
+                    Position = authoring.Transform.localPosition,
+                    Rotation = authoring.Transform.localRotation,
+                    Scale = authoring.Transform.localScale.x
+                },
                 Speed = authoring.Speed
             });
         }
