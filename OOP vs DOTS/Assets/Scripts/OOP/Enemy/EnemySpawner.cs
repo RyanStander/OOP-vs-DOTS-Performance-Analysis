9a32626f7dc3d59f684c6fc8eee2deb183fd1e0d
Ryan <stander.ryan@gmail.com>
2023-06-22 10:35:10 +0200

    burst compile with set seed

--- OOP vs DOTS/Assets/Scripts/OOP/Enemy/EnemySpawner.cs
+++ OOP vs DOTS/Assets/Scripts/OOP/Enemy/EnemySpawner.cs
@@ -7,14 +7,19 @@ namespace OOP.Enemy
         [SerializeField] private GameObject enemyPrefab;
         [SerializeField] private int numberOfEnemies = 10;
         [SerializeField] private float spawnRadius = 3f;
+        [SerializeField] private uint randomSeed = 123;
     
         private void Start()
         {
+            var random = new Unity.Mathematics.Random(randomSeed);
+
             //spawn enemies in a circle randomly around the spawner
             for (var i = 0; i < numberOfEnemies; i++)
             {
-                var spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
-                Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+                var position = new Vector3(random.NextFloat(-spawnRadius, spawnRadius),
+                    random.NextFloat(-spawnRadius, spawnRadius), 0);
+                
+                Instantiate(enemyPrefab, position, Quaternion.identity);
             }
         }
     }
